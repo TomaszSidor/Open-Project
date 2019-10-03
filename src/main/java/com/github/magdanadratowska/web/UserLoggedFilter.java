009f87370ed42b21abadec66a36e87c2c9d27862
Marcin <modsezam@gmial.com>
2019-10-03 19:52:04 +0200

    change UserLoggedFilter, add valid paths to css and js

--- src/main/java/com/github/magdanadratowska/web/UserLoggedFilter.java
+++ src/main/java/com/github/magdanadratowska/web/UserLoggedFilter.java
@@ -11,11 +11,15 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@WebFilter (urlPatterns = {"/*"}, filterName = "UserLoggedFilter")
+@WebFilter (urlPatterns = {"/*"}, filterName = "UserLoggedFilter", initParams = {})
 public class UserLoggedFilter implements Filter {
 
     private static final Set<String> ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(
-            Arrays.asList("", "/login", "/books/list", "/css/*", "/js/*")));
+            Arrays.asList("", "/login", "/books/list")));
+
+    private static final Set<String> ALLOWED_PATHS_CSS_JS = Collections.unmodifiableSet(new HashSet<>(
+            Arrays.asList("/css/materialize.css", "/css/materialize.min.css",
+                    "/js/materialize.js" , "/css/materialize.min.js")));
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -31,8 +35,9 @@ public class UserLoggedFilter implements Filter {
 
         boolean loggedIn = (session.getAttribute("userName")!=null);
         boolean allowedPath = ALLOWED_PATHS.contains(path);
+        boolean allowedPathCssJs = ALLOWED_PATHS_CSS_JS.contains(path);
 
-        if (loggedIn || allowedPath) {
+        if (loggedIn || allowedPath || allowedPathCssJs) {
             filterChain.doFilter(req, resp);
         } else {
             resp.sendRedirect(req.getContextPath() + "/login");
