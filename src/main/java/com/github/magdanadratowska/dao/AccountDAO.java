16a3210478641a0267d7e7d5f7413e0187a2bed6
Marcin <modsezam@gmial.com>
2019-10-09 20:07:51 +0200

    search feature on book list

--- src/main/java/com/github/magdanadratowska/dao/AccountDAO.java
+++ src/main/java/com/github/magdanadratowska/dao/AccountDAO.java
@@ -25,6 +25,7 @@ public class AccountDAO {
     private static final String REMOVE_BOOK_FROM_USER_LIST = "delete from user_book where id_user=? and id_book=?";
     private static final String SELECT_USERS_LIST = "select * from (select * from user_book, book where (user_book.id_book = book.id AND is_active=true)) T where id_user =?";
     private static final String SELECT_ALL_BOOKS_LIST_FOR_CURRENT_USER_WITH_DELETED_BOOKS = "select * from book B left join (select id as id2, is_active from (select * from book, user_book where (user_book.id_book = book.id)) T where id_user =?) U on (U.id2 = B.id) limit ?, ?";
+    private static final String SELECT_ALL_BOOKS_LIST_FOR_CURRENT_USER_WITH_QUERY = "select * from book B left join (select id as id2, is_active from (select * from book, user_book where (user_book.id_book = book.id)) T where id_user =?) U on (U.id2 = B.id) where lower(title) like lower(?) limit ?, ?;";
     private static final String COUNT_SELECT_ALL_BOOKS = "select count(*) as count from book";
     private static final String SELECT_BOOK_DETAIL_FOR_CURRENT_USER = "select * from (select * from book where book.id =?) T left join (select * from user_book where id_user =?) L on (T.id = L.id_book);";
     private static final String DELETE_BOOK_FROM_USER_LIST = "update user_book set is_active = false WHERE (id_user=? AND id_book=?)";
@@ -103,6 +104,32 @@ public class AccountDAO {
             preparedStatement.setLong(1, user.getId());
             preparedStatement.setLong(2, limitFrom);
             preparedStatement.setLong(3, numberOfRows);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                String authorName = resultSet.getString("author_name");
+                String authorSurname = resultSet.getString("author_surname");
+                Book book = new Book(id, title, authorName, authorSurname);
+                boolean isActive = resultSet.getBoolean("is_active");
+                boolean isOwned = resultSet.getBoolean("id2");
+                usersBookList.add(new UserBook(book, isActive, isOwned));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usersBookList;
+    }
+
+    public List<UserBook> getAllBookListForCurrentUserWithQuery(User user, String query, long limitFrom, long numberOfRows) {
+        List<UserBook> usersBookList = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_BOOKS_LIST_FOR_CURRENT_USER_WITH_QUERY);) {
+            preparedStatement.setLong(1, user.getId());
+            preparedStatement.setString(2, "%" + query + "%");
+            preparedStatement.setLong(3, limitFrom);
+            preparedStatement.setLong(4, numberOfRows);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 long id = resultSet.getLong("id");
